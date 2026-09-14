73ff1ccbf0c5eab6ffa1f583908f6d4205ed490c
Anmol Tyagi <tyagiji2005@gmail.com>
2026-09-14 13:39:32 +0530

    Day 03 done

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+
 const healthRoutes = require("./routes/health.routes");
+const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/health", healthRoutes);
+app.use("/api/users", userRoutes);
 
 module.exports = app;
---
+++ server/src/controllers/user.controller.js
@@ -0,0 +1,25 @@
+const User = require("../models/user.model");
+
+const createUser = async (req, res) => {
+    try {
+        const user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        res.status(201).json({
+            success: true,
+            data: user
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
+
+module.exports = {
+    createUser
+};
